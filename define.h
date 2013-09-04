0c2b2bbe016228f18b4408631bdd4e36c11af888
Lancelot <wangye0206@gmail.com>
2013-09-04 13:01:07 -0400

    Add options

---
+++ define.h
@@ -0,0 +1,10 @@
+#ifndef DEFINE_H_
+#define DEFINE_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+enum job {COMPILE, CLEAN, HELP};
+
+
+#endif
---
+++ initialization.h
@@ -0,0 +1,11 @@
+#ifndef INITIALIZATION_H_
+#define INITIALIZATION_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "define.h"
+#include "environment.h"
+
+extern job Initialization( environment_variable &ev, int argc, char** argv);
+
+#endif
